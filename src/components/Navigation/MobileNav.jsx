287ab3b9145fafab7dd77157ae33471e72b15fda
adeka stanley <adekastanley2@gmail.com>
2023-11-20 19:41:33 +0100

    create mobile nav, animations left and a bit of more styling to finish uo

--- src/components/Navigation/MobileNav.jsx
+++ src/components/Navigation/MobileNav.jsx
@@ -1,30 +1,52 @@
+import { useState } from "react";
 import logo from "../../assets/images/logo.png";
+import { motion } from "framer-motion";
 const MobileNav = () => {
 	const navLinks = [
 		{ title: "Home", href: "/" },
 		{ title: "About", href: "/" },
 		{ title: "Contact", href: "/" },
+		{ title: "Get started", href: "/" },
 	];
+	const [openMenu, setOpenMenu] = useState(false);
+	const toggleMenu = () => {
+		setOpenMenu(!openMenu);
+	};
 	return (
 		<div className="mobile-nav-wrapper">
 			<div className="mobile-control-wrapper">
 				<div className="logo-div">
 					<img src={logo} alt="oyanow logo" />
 				</div>
-				<div className="menu-div">menu</div>
+				<div className="menu-btn" onClick={toggleMenu}>
+					menu
+				</div>
 			</div>
 
-			<nav className="mobile-nav">
-				<ul className="nav-links-wrapper">
-					{navLinks.map((link, index) => {
-						return (
-							<li key={index}>
-								<a href={link.href}>{link.title}</a>
-							</li>
-						);
-					})}
-				</ul>
-			</nav>
+			{openMenu && (
+				<nav className="mobile-nav">
+					<div className="displayed-mobile-menu-wrapper">
+						<div className="mobile-control-wrapper">
+							<div className="logo-div">
+								<img src={logo} alt="oyanow logo" />
+							</div>
+							<div className="menu-btn" onClick={toggleMenu}>
+								close
+							</div>
+						</div>
+					</div>
+
+					<ul className="nav-links-wrapper">
+						{navLinks.map((link, index) => {
+							return (
+								<li key={index}>
+									<a href={link.href}>{link.title}</a>
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			)}
 		</div>
 	);
 };
